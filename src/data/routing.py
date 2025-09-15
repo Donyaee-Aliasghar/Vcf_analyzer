47f45d32b3a0dadf7377db6881cb3509c6fd6b89
Donyaee-Aliasghar <donyaeealiasghar@gmail.com>
2025-09-15 18:37:51 +0330

    Refactor(routing): changing in geter pure file and minimize path movements.

--- src/data/routing.py
+++ src/data/routing.py
@@ -2,6 +2,24 @@
 
 from pathlib import Path
 
+# Root dir.
+ROOT_DIR = Path(__file__).resolve().parents[2]
+
+# Results dir.
+RESULTS_DIE = ROOT_DIR / "results"
+
 # Output dir for purified raw data.
-OUTPUT_PURIFIED_DATA_DIR = Path(__file__).resolve().parents[2] / "results/data/purified"
+OUTPUT_PURIFIED_DATA_DIR = RESULTS_DIE / "data/purified"
 OUTPUT_PURIFIED_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+# Output dir for basic analyse.
+OUTPUT_BASIC_ANALYSE_DIR = RESULTS_DIE / "basic_analyse"
+OUTPUT_BASIC_ANALYSE_DIR.mkdir(parents=True, exist_ok=True)
+
+# Get pure file of results directory.
+vcf_files = list(OUTPUT_PURIFIED_DATA_DIR.glob("*.vcf"))
+if vcf_files:
+    INPUT_PURE_FILE = vcf_files[0]
+else:
+    INPUT_PURE_FILE = None
+    print(f"[❌] No .vcf file found in {OUTPUT_PURIFIED_DATA_DIR}")
