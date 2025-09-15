25c965cd1abff47b2b331ebd29dfc01101614cf9
Donyaee-Aliasghar <donyaeealiasghar@gmail.com>
2025-09-15 18:35:17 +0330

    Refactor(purificatoin data): changing in output file codes.

--- src/data/purification.py
+++ src/data/purification.py
@@ -19,12 +19,11 @@ def clean_vcf_line(line: str) -> bool:
     return False
 
 
-def process_vcf(input_file):
-    for vcf_file in input_file:
-        clean_file = OUTPUT_PURIFIED_DATA_DIR / ("pure_" + vcf_file.stem + ".vcf")
-        with open_vcf(vcf_file) as fr, open(clean_file, "w") as fw:
-            print("[✅] Open raw VCF file successful.")
-            for line in fr:
-                if clean_vcf_line(line):
-                    fw.write(line)
-            print(f"[✅] Purified VCF file successful. {clean_file}")
+def process_vcf(input_paths):
+    clean_file = OUTPUT_PURIFIED_DATA_DIR / ("purified_" + input_paths.name + ".vcf")
+    with open_vcf(input_paths) as fr, open(clean_file, "w", encoding="utf-8") as fw:
+        print("[✅] Open raw VCF file successful.")
+        for line in fr:
+            if clean_vcf_line(line):
+                fw.write(line)
+        print(f"[✅] Purified VCF file successful. {clean_file}")
